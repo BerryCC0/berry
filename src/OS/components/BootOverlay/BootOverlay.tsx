8d4266b5d474bcf420730d038cc9da80ae637222
wylin <139944534+imwylin@users.noreply.github.com>
2026-02-07 12:23:51 -0800

    hugo loadeing screen

--- src/OS/components/BootOverlay/BootOverlay.tsx
+++ src/OS/components/BootOverlay/BootOverlay.tsx
@@ -7,7 +7,6 @@
  */
 
 import { useEffect, useState } from "react";
-import { getIcon } from "@/OS/lib/IconRegistry";
 import styles from "./BootOverlay.module.css";
 
 interface BootOverlayProps {
@@ -50,43 +49,15 @@ export function BootOverlay({
     return null;
   }
 
-  // Determine status text based on current boot phase
-  let statusText = "Starting Berry OS...";
-  if (!isBooting && isWaitingForWallet) {
-    statusText = "Checking session...";
-  } else if (!isBooting && isLoadingData) {
-    statusText = "Loading your settings...";
-  } else if (!isBooting && !isWaitingForWallet && !isLoadingData && !isReady) {
-    statusText = "Preparing desktop...";
-  }
-
   return (
     <div className={`${styles.overlay} ${fadeOut ? styles.fadeOut : ""}`}>
-      <div className={styles.content}>
-        {/* Berry Logo */}
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img
-          src={getIcon("berry")}
-          alt="Berry OS"
-          className={styles.logo}
-        />
-
-        {/* Berry OS Title */}
-        <h1 className={styles.title}>Berry OS</h1>
-
-        {/* Loading Spinner */}
-        <div className={styles.spinner}>
-          <div className={styles.spinnerDot}></div>
-          <div className={styles.spinnerDot}></div>
-          <div className={styles.spinnerDot}></div>
-        </div>
-
-        {/* Status Text */}
-        <p className={styles.status}>{statusText}</p>
-      </div>
-
-      {/* Retro scan lines effect */}
-      <div className={styles.scanlines}></div>
+      {/* Berry Glyph GIF - centered */}
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
+        src="/icons/berry-glyph.gif"
+        alt="Berry OS"
+        className={styles.glyph}
+      />
     </div>
   );
 }
