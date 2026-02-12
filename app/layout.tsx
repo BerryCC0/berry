b924648fba2ed6e83e726723ba9a7c7147de9b7e
wylin <139944534+imwylin@users.noreply.github.com>
2026-02-12 09:07:32 -0800

    progressive web app fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,6 +38,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="manifest" href="/manifest.json" />
         <link
           href="https://fonts.googleapis.com/css2?family=Comic+Neue:wght@400;700&display=swap"
           rel="stylesheet"
